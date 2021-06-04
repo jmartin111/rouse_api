297e99c72456331b1fdc17881142765deafcb7fd
Jeff <chess.logic@gmail.com>
2021-06-04 11:45:06 -0700

    Move the data to the calculation function (duh)

--- main.py
+++ main.py
@@ -11,22 +11,21 @@ def calc_value(model, year):
     :param: year: model year
     :return: object: market and auction values
     """
-    pass
-
-
-if __name__ == "__main__":
     with open("res/api-response.json", "r") as resp:
         data = json.load(resp)
 
         # scan through and grab valid models
         valid_models = [y for y in data]
-        model = input(f"Enter a model number e.g. {valid_models[0]}: ")
 
         # now get the valid years
         valid_years = [y for y in data[model]["schedule"]["years"]]
-        year = input(f"Enter a year e.g. {valid_years[0]}: ")
 
-        try:
-            calc_value(data[model], data[model]["schedule"][year])
-        except KeyError as error:
-            print(f"{error} is not a valid entry.")
+
+if __name__ == "__main__":
+    model = input(f"Enter a model number: ")
+    year = input(f"Enter a year: ")
+
+    try:
+        calc_value(model, year)
+    except KeyError as error:
+        print(f"{error} is not a valid entry.")

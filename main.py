b5de2fcbdafd0c676197607c915ddb64b86c2851
Jeff <chess.logic@gmail.com>
2021-06-04 12:25:40 -0700

    Cleanup the calc function
    * remove commented out code
    * remove validation code

--- main.py
+++ main.py
@@ -14,18 +14,26 @@ def calc_value(model, year):
     with open("res/api-response.json", "r") as resp:
         data = json.load(resp)
 
-        # scan through and grab valid models
-        valid_models = [y for y in data]
+        cost = data[model]["saleDetails"]["cost"]
+        market_ratio = data[model]["schedule"]["years"][year]["marketRatio"]
+        auction_ratio = data[model]["schedule"]["years"][year]["auctionRatio"]
 
-        # now get the valid years
-        valid_years = [y for y in data[model]["schedule"]["years"]]
+        return {"marketValue": cost * market_ratio,
+                "auctionValue": cost * auction_ratio
+                }
 
 
 if __name__ == "__main__":
+    print("=====AMAZING BOB'S AUCTION HOUSE=====")
     model = input(f"Enter a model number: ")
     year = input(f"Enter a year: ")
 
     try:
-        calc_value(model, year)
+        values = calc_value(model, year)
+        print(f"\nMarket Value : {values['marketValue']:.2f}")
+        print(f"Auction Value: {values['auctionValue']:.2f}")
     except KeyError as error:
         print(f"{error} is not a valid entry.")
+    finally:
+        print("\nThanks for your patronage!!")
+

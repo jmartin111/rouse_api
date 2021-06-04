2e6c5c4570fbb9d3a0499b59a5e02ab6689ef7d9
Jeff <chess.logic@gmail.com>
2021-06-04 13:01:23 -0700

    Add unit test skeleton

--- rouse_api.py
+++ rouse_api.py
@@ -2,21 +2,22 @@
 import json
 
 
-def calc_value(model, year):
+def calc_value(eq_model, eq_year):
     """
     Take the model id and year, and returns an object
     containing the calculated values (Market and Auction).
 
-    :param: model: equipment model
-    :param: year: model year
+    :param: eq_model: equipment model
+    :param: eq_year: equipment year
     :return: object: market and auction values
     """
     with open("res/api-response.json", "r") as resp:
         data = json.load(resp)
 
-        cost = data[model]["saleDetails"]["cost"]
-        market_ratio = data[model]["schedule"]["years"][year]["marketRatio"]
-        auction_ratio = data[model]["schedule"]["years"][year]["auctionRatio"]
+        # TODO: this notation is clunky; fix it
+        cost = data[eq_model]["saleDetails"]["cost"]
+        market_ratio = data[eq_model]["schedule"]["years"][eq_year]["marketRatio"]
+        auction_ratio = data[eq_model]["schedule"]["years"][eq_year]["auctionRatio"]
 
         return {"marketValue": cost * market_ratio,
                 "auctionValue": cost * auction_ratio
@@ -24,16 +25,17 @@ def calc_value(model, year):
 
 
 if __name__ == "__main__":
+    """ Main function """
+    
     print("=====AMAZING BOB'S AUCTION HOUSE=====")
     model = input(f"Enter a model number: ")
     year = input(f"Enter a year: ")
 
     try:
         values = calc_value(model, year)
-        print(f"\nMarket Value : {values['marketValue']:.2f}")
-        print(f"Auction Value: {values['auctionValue']:.2f}")
+        print(f"\nMarket Value : ${values['marketValue']:,.2f}")
+        print(f"Auction Value: ${values['auctionValue']:,.2f}")
     except KeyError as error:
         print(f"{error} is not a valid entry.")
     finally:
         print("\nThanks for your patronage!!")
-
